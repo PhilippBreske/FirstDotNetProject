3e64aa7afec539c8e759ddb8d26cba9514f21bd2
Philipp Breske <philipp.breske@stud.htwk-leipzig.de>
2024-05-30 07:43:20 +0200

    clean up

---
+++ TestProject.Domain/ProductHolder/ProductHolder.cs
@@ -0,0 +1,39 @@
+using TestProject.Domain.ProductModels;
+namespace TestProject.Domain.ProductHolder;
+
+
+
+//Database simulation class for products
+public class ProductHolder {
+    //underscore is a naming convention for private fields
+    private readonly List<Product> _products;
+    
+    //initialize the list with a single product
+    public ProductHolder() {
+        _products = new List<Product>{
+            new Product {
+                Id = 1, Name = "Product 1", Price = 10.0 
+            }
+        };
+    }
+    
+    //return product for given ID
+    public Product? GetProductById(int id) {
+        return _products.FirstOrDefault(p => p.Id == id);
+    }
+    
+    //return all products
+    public List<Product> GetProducts() {
+        return _products;
+    }
+    
+    //add product to the list
+    public void AddProduct(Product product) {
+        _products.Add(product);
+    }
+    
+    //remove product from the list
+    public void RemoveProduct(int id){
+        _products.Remove(_products.First(p => p.Id == id));
+    }
+}
---
+++ TestProject.Domain/ProductModels/Product.cs
@@ -0,0 +1,10 @@
+namespace TestProject.Domain.ProductModels;
+
+
+//Product object
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public double Price { get; set; }
+}
